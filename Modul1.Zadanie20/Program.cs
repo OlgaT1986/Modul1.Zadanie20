87e30ff91e21a7df981fae450dfca87a109c9403
Ольга <evinn86@mail.ru>
2022-02-03 16:59:24 +0700

    Добавьте файлы проекта.

---
+++ Modul1.Zadanie20/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul1.Zadanie20
+{
+    class Program
+    {
+        delegate double Mydelegate(double R);
+        static void Main(string[] args)
+        {
+            Mydelegate mydelegate = LengthCircle;
+            // вызов метода LengthCircle
+            double length = mydelegate(6); 
+            Console.WriteLine("{0:0.00}", length);
+
+            mydelegate = SquareCircle;
+            // вызов метода SquareCircle
+            double square = mydelegate(6); 
+            Console.WriteLine("{0:0.00}", square);
+
+            mydelegate = VolumeBall;
+            // вызов метода VolumeBall
+            double volume = mydelegate(6); 
+            Console.WriteLine("{0:0.00}", volume);
+            Console.ReadKey();
+        }
+        //метод получит R и вычисляет длину окружности 
+        static double LengthCircle(double R) => (double)Math.PI * 2 * R;
+        //метод получит R и вычислит площадь круга
+        static double SquareCircle(double R) => (double)Math.PI * R * R;
+        //метод получит R и вычислит объем шара
+        static double VolumeBall(double R) => (double)4 / 3 * Math.PI * R * R * R;
+    }
+}
+
---
+++ Modul1.Zadanie20/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Modul1.Zadanie20")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Modul1.Zadanie20")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("75ed2c57-627c-49f8-9228-4d0f2ac45b1e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
